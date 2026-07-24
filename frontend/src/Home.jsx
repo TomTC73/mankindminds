b728982200e483db6709390a0d2efc41e7b1635b
TomTC73 <tomcarey23@gmail.com>
2026-07-24 16:03:11 +0100

    Art is now on the website

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,13 +1,19 @@
 import "./Home.css";
 import { Link } from "react-router-dom";
 import Header from "./Header";
+import backgroundImage from "./assets/BackgroundHome.png";
 
 function Home() {
   return (
     <div>
       <Header />
 
-      <section className="hero">
+      <section
+        className="home-hero"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+        }}
+      >
         <div className="hero-box">
           <h2>Human Creativity Certified</h2>
 
@@ -21,7 +27,9 @@ function Home() {
           </Link>
 
           <Link to="/about">
-            <button className="button secondary-button">About Verification</button>
+            <button className="button secondary-button">
+              About Verification
+            </button>
           </Link>
         </div>
       </section>
